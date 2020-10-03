7d7dd1310cb4f9efa68185510a5d3fcb66dbe41d
Patrick Castro <patrickcastro117@yahoo.com>
2020-10-02 17:17:25 -0700

    added log out button

--- includes/header.php
+++ includes/header.php
@@ -14,8 +14,20 @@ if (isset($_SESSION['fname'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="./assets/js/bootstrap.bundle.min.js"></script>
+  <script src="./assets/js/bootstrap.bundle.js"></script>
+  <script src="./assets/js/bootstrap.js"></script>
+  <link rel="stylesheet" href="./assets/css/bootstrap.css">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="./assets/css/font-awesome-4.7.0//css/font-awesome.css">
   <link rel="stylesheet" href="./assets/css/index.css">
+  <link rel="stylesheet" href="./assets/css/header.css">
   <title>Tech Café</title>
 </head>
 <body>
-  alkefj;kj
+  <nav class="header">
+    <a href="includes/handlers/logout.php">
+      <i class="fa fa-sign-out" aria-hidden="true" href=''></i>
+    </a>
+  </nav>
