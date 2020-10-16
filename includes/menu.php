cc15bd63c4baf9470ddbacaed20770c368fa6c6b
Patrick Castro <patrickcastro117@yahoo.com>
2020-10-15 17:08:03 -0700

    added menu items with details and prices

--- includes/menu.php
+++ includes/menu.php
@@ -1,18 +1,110 @@
 <div class="menu">
 
   <div class="hot">
-    <p>Hot Drinks</p>
-    <a href="#" class="flex">
-      <span class="choccy"></span>
-    </a>
+
+    <div class="headline">
+      <p>Hot Drinks</p>
+    </div>
+
+    <div class="products">
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="choccy"></span>
+        </a>
+        <p>Hot Chocolate</p>
+        <p>$2.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="hcoffee"></span>
+        </a>
+        <p>Vanilla Latté</p>
+        <p>$4.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="htea"></span>
+        </a>
+        <p>Hot Peach Tea</p>
+        <p>$3.95</p>
+      </div>
+
+    </div>
+
   </div>
 
   <div class="cold">
-    <p>Cold Drinks</p>
+
+    <div class="headline">
+      <p>Cold Drinks</p>
+    </div>
+
+    <div class="products">
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="ichoccy"></span>
+        </a>
+        <p>Iced Chocolate</p>
+        <p>$2.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="icoffee"></span>
+        </a>
+        <p>Iced Vanilla Latté</p>
+        <p>$4.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="itea"></span>
+        </a>
+        <p>Iced Lemon Tea</p>
+        <p>$3.95</p>
+      </div>
+
+    </div>
+
   </div>
 
   <div class="beans">
-    <p>Coffee Beans</p>
-  </div>
 
+    <div class="headline">
+      <p>Coffee Beans</p>
+    </div>
+
+    <div class="products">
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="light"></span>
+        </a>
+        <p>Light Roast</p>
+        <p>$10.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="medium"></span>
+        </a>
+        <p>Medium Ethiopian</p>
+        <p>$10.95</p>
+      </div>
+
+      <div class="details">
+        <a href="#" class="flex">
+          <span class="dark"></span>
+        </a>
+        <p>Dark Ethiopian</p>
+        <p>$10.95</p>
+      </div>
+
+    </div>
+
+  </div>
 </div>
